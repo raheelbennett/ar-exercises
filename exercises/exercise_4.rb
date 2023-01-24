06b03aed8528034cf91ffe5eba8a4b3a3430c0a3
Raheel Bennett <raheel.bennett@telus.com>
2023-01-24 13:55:48 -0700

    complete exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,18 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown ", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+#Store.find_by returns the first match, Store.where return all the matches
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each {|store|
+puts store.name + " $" + store.annual_revenue.to_s
+}
+
+#(annual_revenue: ..1000000) is for < 1000000, (annual_revenue: 1000000..) is for > 1000000
+@womens_stores_lowRev = Store.where(womens_apparel: true, annual_revenue: ..1000000)
+
+pp @womens_stores_lowRev 
