ec64154205f9ccb4f77d248a992f087376e500b1
Raheel Bennett <raheel.bennett@telus.com>
2023-01-24 14:10:52 -0700

    complete exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,15 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+
+@total_revenue = Store.sum("annual_revenue")
+
+puts "Total Revenue: $ " + @total_revenue.to_s
+
+@average_revenue = Store.average("annual_revenue")
+
+puts "Average Annual Revenue: $ " + @average_revenue.to_s
+
+@store_count_over_1M_revenue = Store.where(annual_revenue: 1000000..).count
+
+puts "Number of stores generating over $1M in annual sales: " + @store_count_over_1M_revenue.to_s
